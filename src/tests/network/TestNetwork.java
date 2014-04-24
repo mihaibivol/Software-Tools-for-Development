7eaa6ff72434f14726f36d1566ad92274e3f9d01
Mihai Bivol <mm.bivol@gmail.com>
2014-04-24 18:54:13 +0300

    Add test class

---
+++ src/tests/network/TestNetwork.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package tests.network;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author mihai
+ *
+ */
+public class TestNetwork {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link network.Network#downloadFile(common.IFile, common.IUser)}.
+	 */
+	@Test
+	public void testDownloadFile() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link network.Network#accept(java.nio.channels.SelectionKey)}.
+	 */
+	@Test
+	public void testAccept() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link network.Network#process(java.util.List)}.
+	 */
+	@Test
+	public void testProcessListOfObject() {
+		fail("Not yet implemented");
+	}
+
+}
